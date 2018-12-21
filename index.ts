556130e86bc5fd07f8e0d4721200ff1196152b4f
chesterkmr <cheskmr@gmail.com>
2018-12-21 21:34:44 +0200

    ts-action-creator

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { Action } from 'redux';
+
+export const makeAction = function<ActionData , ActionType>(type : ActionType) {
+    function creator(payload? : ActionData) {
+        return {type , ...(payload ? {payload} : {})};
+    }
+
+    creator.match = function(action : Action) : action is Action & {payload : ActionData} {
+        return action.type === type;
+    }
+
+    return creator;
+}
